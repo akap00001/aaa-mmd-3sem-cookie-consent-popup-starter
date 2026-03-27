ceb24a5fd97d638547fb5025a1ec40c199e7a872
Akua <akap0001@stud.kea.dk>
2026-03-27 20:38:56 +0100

    Refactor CookieConsentPopup to manage acceptance and closure states

--- src/components/CookieConsentPopup.jsx
+++ src/components/CookieConsentPopup.jsx
@@ -1,23 +1,29 @@
 "use client";
 import { LiaCookieBiteSolid } from "react-icons/lia";
 import { IoIosClose } from "react-icons/io";
+import { useState } from "react";
 
 const CookieConsentPopup = () => {
+  const [isAccepted, setIsAccepted] = useState(false);
+  const [isClosed, setIsClosed] = useState(false);
+
   return (
-    <section className="cookie-consent-popup">
-      <header className="cookie-consent-popup-header">
-        <LiaCookieBiteSolid size={50} />
-        <IoIosClose className="pointer" size={25} />
-      </header>
-      <footer className="cookie-consent-popup-footer">
-        <p className="cookie-consent-popup-paragraph">
-          We use cookies to improve your user experience!
-        </p>
-        <button className="cookie-consent-popup-cta pointer">
-          I like Cookies
-        </button>
-      </footer>
-    </section>
+    !isAccepted &&
+    !isClosed && (
+      <section className="cookie-consent-popup">
+        <header className="cookie-consent-popup-header">
+          <LiaCookieBiteSolid size={50} />
+          <IoIosClose onClick={() => setIsClosed(!isClosed)} className="pointer" size={25} />
+        </header>
+        <footer className="cookie-consent-popup-footer">
+          <p className="cookie-consent-popup-paragraph">We use cookies to improve your user experience!</p>
+
+          <button onClick={() => setIsAccepted(!isAccepted)} className="cookie-consent-popup-cta pointer">
+            I like Cookies
+          </button>
+        </footer>
+      </section>
+    )
   );
 };
 
